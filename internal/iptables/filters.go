d0191b79245a504cccf2519805cabc4fccb8b830
Matt Johnson <matt9j@cs.washington.edu>
2018-08-09 15:24:01 +0900

    [iptables] Check to not double add rules

--- internal/iptables/filters.go
+++ internal/iptables/filters.go
@@ -7,7 +7,18 @@ import (
 )
 
 func EnableForwardingFilter(addr net.IP) {
-    cmd := exec.Command("iptables", "-I", "FORWARD", "-s", addr.String(), "-j", "REJECT")
+    // Check if the rule is already present before adding another one! IPTables holds state outside the lifetime of this
+    // program. The `-C` option will return success if the rule is present, and 1 if it is not.
+    cmd := exec.Command("iptables", "-C", "FORWARD", "-s", addr.String(), "-j", "REJECT")
+    err := cmd.Run()
+    if err == nil {
+        // The rule is already present in the chain! Do not double insert, as this will require delete to run multiple
+        // times and break the delete implementation
+        log.WithField("address", addr).Warn("Filter rule already present.")
+        return
+    }
+
+    cmd = exec.Command("iptables", "-I", "FORWARD", "-s", addr.String(), "-j", "REJECT")
     //cmd := exec.Command("echo", "Filtered IP " + addr.String())
     output, err := cmd.CombinedOutput()
     if err != nil {
