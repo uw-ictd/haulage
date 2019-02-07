a60e858ac478c937b6cef2d8317c3652a9f645bd
Chenyang Fang <michaelfang@D-10-18-206-44.dhcp4.washington.edu>
2019-02-07 10:25:42 -0800

    added testRadius which includes a fake client that we can use for testing the radius server

---
+++ testRadius/main.go
@@ -0,0 +1,52 @@
+// client-side test
+// RADIUS SERVER TEST
+// Chenyang Fang
+package main
+
+import (
+	"context"
+	"log"
+
+	"layeh.com/radius"
+	"layeh.com/radius/rfc2865"
+	
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("test start ... \n")
+	packet := radius.New(radius.CodeAccessRequest, []byte(`secret`))
+	rfc2865.UserName_SetString(packet, "kurtis")
+	rfc2865.UserPassword_SetString(packet, "funnyGuy")
+	response, err := radius.Exchange(context.Background(), packet, "localhost:1812")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Code:", response.Code)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
