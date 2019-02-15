79bbd02b0ca8554f9f5657560f91e17775798005
Chenyang Fang <michaelfang@Chenyangs-MacBook-Pro.local>
2019-02-15 11:01:48 -0800

    some bugs about if-else needs to be fixed

--- radius.go
+++ radius.go
@@ -4,13 +4,18 @@ package main // what should we name it?
 import (
 	"layeh.com/radius"
 	"layeh.com/radius/rfc2865"
-	
+	"net"
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 
 )
+
+// confusions:
+// q1: is that possible for an imsi that does not have an ip?
+// q2: what should we set the secret
+
 
 // parameter db: the sql database of all mobile users' information within our network
 func start_radius_server(db *sql.DB) {
@@ -22,22 +27,44 @@ func start_radius_server(db *sql.DB) {
 		// grabbing information
 		// note that we assume username = imsi
 		// optional ip is undefined
-		username := rfc2865.UserName_GetString(r.Packet)
-		// password := rfc2865.UserPassword_GetString(r.Packet)
-		var code radius.Code
-		_, err := db.Query("SELECT *  FROM customers WHERE customers.imsi = ?", username)
+		imsi := rfc2865.UserName_GetString(r.Packet)
+		// query through database to find if this imsi matches some IP
+		// if it does, write back the IP
+		// Reject otherwise
+		rows, err := db.Query("SELECT ip FROM static_ips WHERE imsi = ?", imsi)
+		defer rows.Close()
+		var res *radius.Packet
 		if err != nil {
+			// we should reject the user 
 			log.WithError(err).Error("Cannot find this customer in our db")
-			code = radius.CodeAccessReject
+			res = radius.New(radius.CodeAccessReject, []byte(`res`)) // what should we set the secret?
 		} else {
-			code = radius.CodeAccessAccept
+			// we should accept the user
+			res = radius.New(radius.CodeAccessAccept, []byte(`res`)) // what should we set the secret?
+			// grab the IP
+			var ip string
+			for rows.Next() {
+				if err1 := rows.Scan(&ip); err1 != nil {
+        			log.Fatal(err1)
+    			}
+			}
+
+			log.Printf("The IP we found for imsi: %s is IP: %s", imsi, ip)
+			// transform ip from string to IPv4 form
+			IP_found := net.ParseIP(ip)
+			// add the ip to FramedIPAddress attribute of our packet
+			err2 := rfc2865.FramedIPAddress_Add(res, IP_found)
+			if (err2 != nil) {
+				log.Fatal("Cannot add the ip to the packet")
+				// we should handle this situation
+				// what to send back then?
+			}
+		}	
+
+		err = w.Write(res)	
+		if err != nil {
+			log.Fatal("Cannot write back the message")
 		}
-
-	    //code = radius.CodeAccessAccept
-	    //code = radius.CodeAccessReject
-
-	    log.Printf("Writing %v to %v", code, r.RemoteAddr)	
-		w.Write(r.Response(code))
 	}	
 
 	server := radius.PacketServer{
