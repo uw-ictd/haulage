ac34ac9dd22f5bbf73c1162b1466dbb724e4363e
Matt Johnson <matt9j@cs.washington.edu>
2019-03-03 13:48:37 -0800

    [sql] Update SQL types
    After having time to consult the documentation, it's more flexible to
    use datetime instead of timestamp (it avoids timezone issues) and ip
    address and ttl can be represented in the more compact ascii character
    set. This allows for display of the values in the CLI more easily as
    well, versus raw varbinary. The binary encoded IP addresses should
    still be stored as binary for the time being.

--- haualge.sql
+++ haualge.sql
@@ -3,18 +3,21 @@ DROP TABLE IF EXISTS `answers`;
 DROP TABLE IF EXISTS `flowlogs`;
 
 CREATE TABLE `answers` (
-  `host` varbinary(255) NOT NULL,
-  `ip_addresses` varbinary(512) NOT NULL,
-  `ttls` varbinary(255) NOT NULL,
+  `host` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `ip_addresses` varchar(512) CHARACTER SET ascii NOT NULL,
+  `ttls` varchar(255) CHARACTER SET ascii NOT NULL,
   idx bigint UNSIGNED AUTO_INCREMENT,
   UNIQUE KEY (host, ip_addresses, ttls),
   PRIMARY KEY (idx)
 ) ENGINE=InnoDB;
 
 CREATE TABLE dnsResponses (
-  time timestamp NOT NULL,
-  src_ip binary(16) NOT NULL,
-  dest_ip binary(16) NOT NULL,
+  time DATETIME NOT NULL,
+  `srcIp` varbinary(16) NOT NULL,
+  `dstIp` varbinary(16) NOT NULL,
+  `transportProtocol` tinyint UNSIGNED NOT NULL,
+  `srcPort` smallint UNSIGNED NOT NULL,
+  `dstPort` smallint UNSIGNED NOT NULL,
   opcode int UNSIGNED NOT NULL,
   resultcode int UNSIGNED NOT NULL,
   answer bigint UNSIGNED NOT NULL,
@@ -22,8 +25,8 @@ CREATE TABLE dnsResponses (
 ) ENGINE=InnoDB;
 
 CREATE TABLE `flowlogs` (
-  `intervalStart` timestamp NOT NULL,
-  `intervalStop` timestamp NOT NULL,
+  `intervalStart` DATETIME NOT NULL,
+  `intervalStop` DATETIME NOT NULL,
   `addressA` varbinary(16) NOT NULL,
   `addressB` varbinary(16) NOT NULL,
   `transportProtocol` tinyint UNSIGNED NOT NULL,
